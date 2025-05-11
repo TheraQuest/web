e2562e5dd022df41e3fe524588cc929afa153d47
linoynoe <linnoynoe1412@gmail.com>
2025-05-11 13:09:14 +0300

    Update API URLs from localhost to Render for deployment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
         const fetchTherapist = async () => {
             try {
                 const token = localStorage.getItem("token");
-                const response = await axios.get("http://localhost:5000/api/therapist", {
+               const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/therapist`, {
                     headers: { Authorization: `Bearer ${token}` }
                 });
                 console.log("Therapist response:", response.data); 
